ee443427707e9dd8000283fffe0075fb048cfe30
Ignacio Rueda Rodriguez <irueda@alpha28.aulas.gsyc.urjc.es>
2017-10-02 19:33:40 +0200

    añado calc.py y clacplus.py

--- calc.py
+++ calc.py
@@ -4,27 +4,25 @@
 import sys
 
 
-def plus(op1, op2):
-    """ Function to sum the operands. Ops have to be ints """
+def suma(op1, op2):
     return op1 + op2
 
 
-def minus(op1, op2):
-    """ Function to substract the operands """
+def resta(op1, op2):
     return op1 - op2
 
 if __name__ == "__main__":
     try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
+        valor1 = int(sys.argv[1])
+        valor2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
     if sys.argv[2] == "suma":
-        result = plus(operando1, operando2)
+        resultado = suma(operando1, operando2)
     elif sys.argv[2] == "resta":
-        result = minus(operando1, operando2)
+        resultado = resta(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
-    print(result)
+    print(resultado)
